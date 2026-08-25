57d72932643dc5862c7f34894a7713d4305ba74d
Lusine Sargsyan <lusinesargsyanh2@gmail.com>
2026-08-25 19:02:11 +0000

    class to function

---
+++ practiceClassToFunction/classAsFunction.js
@@ -0,0 +1,159 @@
+// Animal
+function Animal(name, age, energy = 100) {
+    this._name = name;
+    this._age = age;
+    this._energy = energy;
+}
+Animal.prototype.getName = function () {
+    return this._name;
+}
+Animal.prototype.setName = function (value) {
+    this._name = value;
+}
+Animal.prototype.getAge = function () {
+    return this._age;
+}
+Animal.prototype.setAge = function (value) {
+    this._age = value;
+}
+Animal.prototype.getEnergy = function () {
+    return this._energy;
+}
+Animal.prototype.setEnergy = function (value) {
+    this._energy = Math.max(0, Math.min(100, value));
+}
+Animal.prototype.eat = function () {
+    this.setEnergy(this.getEnergy() + 20);
+}
+Animal.prototype.sleep = function (hours) {
+    this.setEnergy(this.getEnergy() + hours * 10);
+}
+Animal.prototype.getInfo = function () {
+    return `${this._name} is ${this._age} year old have energy is ${this._energy}`;
+}
+// Animal end 
+// Predator
+function Predator(name, age, isHungry = true) {
+    Animal.call(this, name, age);
+    this._isHungry = isHungry;
+}
+
+Predator.__proto__ = Animal;
+Predator.prototype = Object.create(Animal.prototype); // Predator.prototype.__proto__ --> Animal.prototype
+Predator.prototype.constructor = Predator;
+
+Predator.prototype.hunt = function () {
+    this.setEnergy(this.getEnergy() - 30);
+}
+Predator.prototype.getInfo = function () {
+    return `${this._name} is ${this._age} year old have energy is ${this._energy}  and  ${this._isHungry ? "is" : "isn't"} hungry`;
+}
+
+Predator.prototype.getIsHungry = function () {
+    return this._isHungry;
+}
+Predator.prototype.setIsHungry = function (value) {
+    this._isHungry = value
+}
+
+// Predator end
+// Lion
+function Lion(name, age, prideName) {
+    Predator.call(this, name, age);
+    this._prideName = prideName;
+}
+
+
+Lion.__proto__ = Predator
+Lion.prototype = Object.create(Predator.prototype); // Lion.prototype.__proto__ --> Predator.prototype
+Lion.prototype.constructor = Lion;
+
+Lion.prototype.getPrideName = function () {
+    return this._prideName;
+}
+Lion.prototype.setPrideName = function (value) {
+    this._prideName = value
+}
+Lion.prototype.roar = function () {
+    this.setEnergy(this.getEnergy() - 10);
+}
+// Lion end
+// Herbivore
+function Herbivore(name, age, favoritePlant) {
+    Animal.call(this, name, age);
+    this._favoritePlant = favoritePlant;
+}
+Herbivore.__proto__ = Animal;
+Herbivore.prototype = Object.create(Animal.prototype); // Herbivore.prototype.__proto__ --> Animal.prototype
+Herbivore.prototype.constructor = Herbivore;
+
+Herbivore.prototype.getFavoritePlant = function () {
+    return this._favoritePlant;
+}
+Herbivore.prototype.setFavoritePlant = function (value) {
+    this._favoritePlant = value
+}
+Herbivore.prototype.graze = function () {
+    this.setEnergy(this.getEnergy() - 10);// searching for food
+    this.setEnergy(this.getEnergy() + 15);// eating restores energy
+}
+Herbivore.prototype.getInfo = function () {
+    return `${this._name} is ${this._age} year old have energy is ${this._energy} and favorite plant is  ${this._favoritePlant}`;
+}
+// Herbivore end
+// Rabbit
+function Rabbit(name, age, favoritePlant, jumpHeight) {
+    Herbivore.call(this, name, age, favoritePlant);
+    this._jumpHeight = jumpHeight;
+}
+
+Rabbit.__proto__ = Herbivore
+Rabbit.prototype = Object.create(Herbivore.prototype); // Rabbit.prototype.__proto__ --> Herbivore.prototype
+Rabbit.prototype.constructor = Rabbit;
+
+Rabbit.prototype.getJumpHeight = function () {
+    return this._jumpHeight;
+}
+Rabbit.prototype.setJumpHeight = function (value) {
+    this._jumpHeight = value
+}
+Rabbit.prototype.graze = function () {
+    this.setEnergy(this.getEnergy() - 5);// searching for food
+    this.setEnergy(this.getEnergy() + 20);// eating restores energy
+}
+
+Rabbit.prototype.jump = function () {
+    this.setEnergy(this.getEnergy() - 5);
+}
+
+// Rabbit end
+
+
+
+const lion = new Lion("Simba", 10, "Simba's pride");
+
+
+console.log(lion.getEnergy()); //100
+lion.eat; // not change energy because max energy is 100
+console.log(lion.getEnergy()); // 100 
+lion.hunt();
+console.log(lion.getEnergy());
+console.log(lion.getInfo());
+
+lion.eat()
+console.log(lion.getEnergy());
+lion.setIsHungry(false)
+console.log(lion.getInfo());
+
+const rabbit = new Rabbit("Judy Hopps", 24, "carrot", 40);
+
+console.log("Rabbit example");
+console.log(rabbit.getInfo());
+console.log(rabbit.getEnergy());
+rabbit.jump()
+rabbit.jump()
+rabbit.jump()
+rabbit.jump()
+console.log(rabbit.getEnergy());
+rabbit.graze()
+console.log(rabbit.getEnergy());
---
+++ practiceClassToFunction/classExample.js
@@ -0,0 +1,166 @@
+class Animal {
+    #name;
+    #age;
+    #energy;
+
+    constructor(name, age, energy = 100) {
+        this.#age = age;
+        this.#name = name;
+        this.#energy = energy;
+    }
+
+    get name() {
+        return this.#name;
+    }
+    set name(value) {
+        this.#name = value;
+    }
+    get age() {
+        return this.#age;
+    }
+    set age(value) {
+        this.#age = value;
+    }
+    get energy() {
+        return this.#energy;
+    }
+    set energy(value) {
+        this.#energy = Math.max(0, Math.min(100, value));
+    }
+
+    eat() {
+        this.energy += 20;
+    }
+    sleep(hours) {
+        this.energy += hours * 10;
+    }
+    getInfo() {
+        return `${this.name} is ${this.age} year old have energy is ${this.energy}`;
+    }
+
+}
+
+class Predator extends Animal {
+    #isHungry;
+    constructor(name, age, isHungry = true) {
+        super(name, age);
+        this.#isHungry = isHungry;
+    }
+
+    get isHungry() {
+        return this.#isHungry;
+    }
+    set isHungry(value) {
+        this.#isHungry = value;
+    }
+
+    hunt() {
+        this.energy -= 30;
+    }
+    getInfo() {
+        return `${super.getInfo()} and  ${this.isHungry ? "is" : "isn't"} hungry`;
+    }
+}
+
+class Lion extends Predator {
+    #prideName
+    constructor(name, age, prideName) {
+        super(name, age);
+        this.#prideName = prideName;
+    }
+
+    get prideName() {
+        return this.#prideName;
+    }
+    set prideName(value) {
+        this.#prideName = value;
+    }
+    roar() {
+        this.energy -= 10;
+    }
+    hunt() {
+        super.hunt();
+    }
+
+}
+
+class Herbivore extends Animal {
+    #favoritePlant;
+
+    constructor(name, age, favoritePlant) {
+        super(name, age);
+        this.#favoritePlant = favoritePlant;
+
+    }
+    get favoritePlant() {
+        return this.#favoritePlant;
+    }
+    set favoritePlant(value) {
+        this.#favoritePlant = value;
+    }
+    graze() {
+        this.energy -= 10; // searching for food
+        this.energy += 15; // eating restores energy
+    }
+    getInfo() {
+        return `${super.getInfo()} and favorite plant is  ${this.favoritePlant}`;
+    }
+}
+
+class Rabbit extends Herbivore {
+    #jumpHeight;
+    constructor(name, age, favoritePlant, jumpHeight) {
+        super(name, age, favoritePlant);
+        this.#jumpHeight = jumpHeight;
+    }
+
+    get jumpHeight() {
+        return this.#jumpHeight;
+    }
+    set jumpHeight(value) {
+        this.#jumpHeight = value;
+    }
+    graze() {
+        this.energy -= 5; // searching for food
+        this.energy += 20; // eating restores energy
+    }
+    jump() {
+        this.energy -= 5;
+    }
+}
+
+
+
+const lion = new Lion("Simba", 10, "Simba's pride");
+
+
+console.log(lion.energy); //100
+lion.eat(); // not change energy because max energy is 100
+console.log(lion.energy); // 100 
+lion.hunt();
+console.log(lion.energy);
+console.log(lion.getInfo());
+
+lion.eat();
+console.log(lion.energy);
+lion.isHungry = false;
+
+console.log(lion.getInfo());
+
+
+const rabbit = new Rabbit("Judy Hopps", 24, "carrot", 40);
+console.log("Rabbit example");
+
+console.log(rabbit.getInfo());
+console.log(rabbit.energy);
+rabbit.jump()
+rabbit.jump()
+rabbit.jump()
+rabbit.jump()
+console.log(rabbit.energy);
+rabbit.graze()
+console.log(rabbit.energy);
+
+
+
+
